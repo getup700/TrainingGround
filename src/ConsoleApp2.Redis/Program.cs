562a1bc8337b0af35c2abdaa419082340e53927c
Omar <2609639898@qq.com>
2025-04-13 23:11:14 +0800

    添加redis服务

---
+++ src/ConsoleApp2.Redis/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+var redis = ConnectionMultiplexer.Connect("39.106.54.114");
+var db = redis.GetDatabase();
+
+//db.StringSet("myKey", "Hello,Redis!");
+var value = db.StringGet("myKey");
+Console.WriteLine(value);
+
+redis.Close();
