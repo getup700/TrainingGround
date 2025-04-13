0c0667347c54266542ad065f06d7903359ca9220
Omar <2609639898@qq.com>
2025-04-13 21:07:03 +0800

    添加signalr客户端

---
+++ WpfApp.Communication/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Communication
+{
+    internal class Message
+    {
+        public Message(string content)
+        {
+            Content = content;
+        }
+        public string Content { get; set; }
+
+        public DateTime CreateTime { get; } = DateTime.Now;
+    }
+}
