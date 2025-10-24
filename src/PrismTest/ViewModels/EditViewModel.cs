7d659ea17ddc2c4503a796fae4096f3dd4bf5b02
hzw <getup700@noreply.gitcode.com>
2025-10-24 08:04:31 +0800

    添加prism测试项目。同一view，不同viewmodel子类导航有bug，永远都导向最后一个注册的vm

---
+++ src/PrismTest/ViewModels/EditViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PrismTest.ViewModels
+{
+    class EditViewModel : ViewModelBase, IDialogAware
+    {
+        public EditViewModel() : base()
+        {
+            Title = "Edit";
+            EditCommand = new DelegateCommand(Edit);
+        }
+
+        public ICommand EditCommand { get; }
+
+        public DialogCloseListener RequestClose { get; }
+
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+
+        }
+
+        private void Edit()
+        {
+            MessageBox.Show(Title);
+        }
+    }
+}
---
+++ src/PrismTest/ViewModels/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PrismTest.ViewModels
+{
+    class ViewModelBase:BindableBase
+    {
+        public ViewModelBase()
+        {
+			GoCommand = new DelegateCommand(Go);
+        }
+
+        private string _title;
+
+		public string Title
+		{
+			get { return _title; }
+			set { _title = value;RaisePropertyChanged(); }
+		}
+
+		public ICommand GoCommand { get; }
+		private void Go()
+		{
+			MessageBox.Show(Title);
+		}
+	}
+}
